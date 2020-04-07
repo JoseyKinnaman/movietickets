7528cdb7133f87ec29d9a23036f139c9d586bac3
NeverSettels <settels.eric@gmail.com>
2020-04-07 12:01:19 -0700

    done the NeverSettels way

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+// MovieTicket Business Logic
+function MovieTicket(title, time, rating) {
+  this.price = 7;
+  this.title = title;
+  this.time = time;
+  this.rating = rating;
+}
+
+
+MovieTicket.prototype.calcPrice = function () {
+  if (this.time <= 5) {
+    this.price += -2
+  }
+  if (this.title === 'Terminator 2' || this.title === 'Die Hard') {
+    this.price += 2;
+  }
+  return this.price;
+}
+
+
+// User Interface Logic
+$(document).ready(function () {
+  var movieArr = [{ name: 'Terminator 2', rating: "R" }, { name: 'Mad Max', rating: "R" }, { name: 'RoboCop', rating: "R" }, { name: 'Die Hard', rating: "R" }]
+  movieArr.forEach(function (movie) {
+    $('#movies').append(`<option value="${movie.name},${movie.rating}">${movie.name}</option>`)
+  })
+  $('#form').submit(function (e) {
+    e.preventDefault();
+    var movieSelection = $('#movies').val().split(',');
+    var time = $('#time').val();
+    var newTicket = new MovieTicket(movieSelection[0], time, movieSelection[1]);
+    alert(newTicket.calcPrice())
+  })
+});
